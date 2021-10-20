2c0a2d46ae6c2fd5599993da92cb754b2ce2b6e2
RakhiPareek268 <87005355+RakhiPareek268@users.noreply.github.com>
2021-10-20 19:42:57 +0530

    Update SortingTest.scala

--- src/test/scala/edu/knoldus/SortingTest.scala
+++ src/test/scala/edu/knoldus/SortingTest.scala
@@ -15,9 +15,9 @@ class SortingTest extends AnyFunSuite {
     assert(sorting.selectionSort(array) === array.sortBy(a => a))
   }
 
-//  test("Test Bubble Sort") {
-//    val array: Array[Int] = Array(3, 4, 5, 3, 2, 1)
-//    assert(sorting.bubbleSort(array) === array.sortBy(a=>a))
-//  }
+test("Test Bubble Sort") {
+  val array: Array[Int] = Array(3, 4, 5, 3, 2, 1)
+  assert(sorting.bubbleSort(array) === array.sortBy(a=>a))
+ }
 
 }
